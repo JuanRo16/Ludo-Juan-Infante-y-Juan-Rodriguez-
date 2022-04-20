0bd8e1ad428f6e4af457f56299a28ae9862507b3
JuanRo16 <98895085+JuanRo16@users.noreply.github.com>
2022-04-20 09:28:37 -0500

    Update and rename Base.py to Bases.py

---
+++ Bases.py
@@ -0,0 +1,60 @@
+
+class baseamarilla:                               
+    rap = None
+
+    def __init__(self, num=-1, x=0, y=0, x0=0, y0=0, double=False, ):
+        self.num = num                
+        self.x = x                    
+        self.y = y
+        self.x0 = x0
+        self.y0 = y0
+        self.rap = Label(image=logoay, width=20, height=20)        
+        self.double = double                                       
+
+    def swap(self):                     
+        self.rap.place(x=self.x0 + 13, y=self.y0 + 14)
+        
+class baseroja:                               
+    rap = None
+
+    def __init__(self, num=-1, x=0, y=0, x0=0, y0=0, double=False, ):
+        self.num = num                
+        self.x = x                    
+        self.y = y
+        self.x0 = x0
+        self.y0 = y0
+        self.rap = Label(image=logoay, width=20, height=20)        
+        self.double = double                                       
+
+    def swap(self):                     
+        self.rap.place(x=self.x0 + 13, y=self.y0 + 14)
+        
+ class baseverde:                               
+    rap = None
+
+    def __init__(self, num=-1, x=0, y=0, x0=0, y0=0, double=False, ):
+        self.num = num                
+        self.x = x                    
+        self.y = y
+        self.x0 = x0
+        self.y0 = y0
+        self.rap = Label(image=logoay, width=20, height=20)        
+        self.double = double                                       
+
+    def swap(self):                     
+        self.rap.place(x=self.x0 + 13, y=self.y0 + 14)
+        
+  class baseazul:                               
+    rap = None
+
+    def __init__(self, num=-1, x=0, y=0, x0=0, y0=0, double=False, ):
+        self.num = num                
+        self.x = x                    
+        self.y = y
+        self.x0 = x0
+        self.y0 = y0
+        self.rap = Label(image=logoay, width=20, height=20)        
+        self.double = double                                       
+
+    def swap(self):                     
+        self.rap.place(x=self.x0 + 13, y=self.y0 + 14)
