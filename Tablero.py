4d23b5bf502ba7ca12db74fc70e988e6d7c0dba2
JuanRo16 <98895085+JuanRo16@users.noreply.github.com>
2022-04-20 10:08:37 -0500

    Update Tablero.py

--- Tablero.py
+++ Tablero.py
@@ -1,75 +1,74 @@
+from graficar import *
 
-def board():                           
+def tablero():                           
 
     tkinter.messagebox.showinfo(title=None, message="Inicie el juego undiendo cualquier tecla")
     v = 0
     z = 0
 
-    while (v != 300):           #Drawing White boxes
+    while (v != 300):           
         z = 0
         while (z != 150):
-            Label(image=logo, width=46, height=46).place(x=(300 + z), y=(0 + v))
+            Label(image=casilla_blanca, width=46, height=46).place(x=(300 + z), y=(0 + v))
             z = z + 50
         v = v + 50
 
     z = 0
     v = 0
-    while (v != 300):          #Drawing White boxes
+    while (v != 300):          
         z = 0
         while (z != 150):
-            Label(image=logo, width=46, height=46).place(x=(0 + v), y=(300 + z))
+            Label(image=casilla_blanca, width=46, height=46).place(x=(0 + v), y=(300 + z))
             z = z + 50
         v = v + 50
-
-    #####################
 
     v = 0
     z = 0
 
-    while (v != 300):            #Drawing White boxes
+    while (v != 300):         
         z = 0
         while (z != 150):
-            Label(image=logo, width=46, height=46).place(x=(300 + z), y=(450 + v))
+            Label(image=casilla_blanca, width=46, height=46).place(x=(300 + z), y=(450 + v))
             z = z + 50
         v = v + 50
 
     z = 0
     v = 0
-    while (v != 300):         #Drawing White boxes
+    while (v != 300):         
         z = 0
         while (z != 150):
-            Label(image=logo, width=46, height=46).place(x=(450 + v), y=(300 + z))
+            Label(image=casilla_blanca, width=46, height=46).place(x=(450 + v), y=(300 + z))
             z = z + 50
         v = v + 50
 
     v = 0
-    while (v != 250):     #Drawing Green boxes
-        Label(image=logog, width=46, height=46).place(x=(350), y=(50 + v))
+    while (v != 250):     
+        Label(image=base_verde, width=46, height=46).place(x=(350), y=(50 + v))
         v = v + 50
 
-    Label(image=logog, width=46, height=46).place(x=(300), y=(100))
-    Label(image=logogs, width=46, height=46).place(x=(400), y=(50))
+    Label(image=base_verde, width=46, height=46).place(x=(300), y=(100))
+    Label(image=seguro, width=46, height=46).place(x=(400), y=(50))
 
     v = 0
-    while (v != 250):     #Drawing Yellow boxes
-        Label(image=logoy, width=46, height=46).place(x=(450 + v), y=(350))
+    while (v != 250):     
+        Label(image=base_amarilla, width=46, height=46).place(x=(450 + v), y=(350))
         v = v + 50
 
-    Label(image=logoy, width=46, height=46).place(x=(600), y=(300))
-    Label(image=logoys, width=46, height=46).place(x=(650), y=(400))
+    Label(image=base_amarilla, width=46, height=46).place(x=(600), y=(300))
+    Label(image=seguro, width=46, height=46).place(x=(650), y=(400))
 
     v = 0
-    while (v != 250):    #Drawing Red Boxes
-        Label(image=logor, width=46, height=46).place(x=(50 + v), y=(350))
+    while (v != 250):   
+        Label(image=base_azul, width=46, height=46).place(x=(50 + v), y=(350))
         v = v + 50
 
-    Label(image=logor, width=46, height=46).place(x=(100), y=(400))
-    Label(image=logors, width=46, height=46).place(x=(50), y=(300))
+    Label(image=base_azul, width=46, height=46).place(x=(100), y=(400))
+    Label(image=seguro, width=46, height=46).place(x=(50), y=(300))
 
     v = 0
     while (v != 250):    #Drawing Blue Boxes
-        Label(image=logob, width=46, height=46).place(x=(350), y=(450 + v))
+        Label(image=base_roja, width=46, height=46).place(x=(350), y=(450 + v))
         v = v + 50
 
-    Label(image=logobs, width=46, height=46).place(x=(300), y=(650))
-    Label(image=logob, width=46, height=46).place(x=(400), y=(600))
+    Label(image=base_roja, width=46, height=46).place(x=(300), y=(650))
+    Label(image=seguro, width=46, height=46).place(x=(400), y=(600))
